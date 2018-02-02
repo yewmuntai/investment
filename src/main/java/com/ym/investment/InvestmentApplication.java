ca300337f9b8129983215f0d55613ade36658764
Yewmun <yewmun.tai@unchartedpartners.com>
2018-02-02 10:29:33 +0800

    Initial commit

---
+++ src/main/java/com/ym/investment/InvestmentApplication.java
@@ -0,0 +1,12 @@
+package com.ym.investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentApplication.class, args);
+	}
+}
